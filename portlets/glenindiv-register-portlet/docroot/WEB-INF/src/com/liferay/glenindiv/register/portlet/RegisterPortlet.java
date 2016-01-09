f9a3a203e0acb296167ae41333801a17748832b3
Jason <494312263@qq.com>
2016-01-09 15:24:51 +0800

    Create Glenindiv Register Portlet

---
+++ portlets/glenindiv-register-portlet/docroot/WEB-INF/src/com/liferay/glenindiv/register/portlet/RegisterPortlet.java
@@ -0,0 +1,11 @@
+package com.liferay.glenindiv.register.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class RegisterPortlet
+ */
+public class RegisterPortlet extends MVCPortlet {
+ 
+
+}
