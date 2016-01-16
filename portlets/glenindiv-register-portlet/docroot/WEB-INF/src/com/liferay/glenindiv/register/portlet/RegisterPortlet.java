793476cd2efb304cf78801b0468817b0c0ca0a99
Jason Xiang <494312263@qq.com>
2016-01-16 20:24:06 +0800

    Finish SMS verify code function.

--- portlets/glenindiv-register-portlet/docroot/WEB-INF/src/com/liferay/glenindiv/register/portlet/RegisterPortlet.java
+++ portlets/glenindiv-register-portlet/docroot/WEB-INF/src/com/liferay/glenindiv/register/portlet/RegisterPortlet.java
@@ -1,6 +1,20 @@
 package com.liferay.glenindiv.register.portlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 import javax.portlet.PortletException;
 import javax.portlet.PortletRequestDispatcher;
@@ -13,26 +27,32 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
+import com.sun.script.javascript.JSAdapter;
 
 /**
  * Portlet implementation class RegisterPortlet
  */
 public class RegisterPortlet extends MVCPortlet {
 	
-	VerifyCodeService vcs = new VerifyCodeService();
+	//设置唯一的apikey值
+    private static String apikey = "54ab286a4a8ec11573608ae2ed822bbe";
 	
+	VerifyCodeService vcs = new VerifyCodeService();
+ 
+	JavaSmsApi javaSmsApi = new JavaSmsApi();
+    
 	@Override
 	public void serveResource(ResourceRequest resourceRequest, ResourceResponse resourceResponse)
 			throws IOException, PortletException {
-		System.out.println("#############AJAX CALL####################");
-		String userName = ParamUtil.getString(resourceRequest, "userName");
- 
-        System.out.println("The value of site parameter sent by AJAX  "+userName);
+		String mobile = ParamUtil.getString(resourceRequest, "userName");
 		String imgString = vcs.createCode();
 		String verifyCode = vcs.getCode();
+		String text = "【GLENINDIV】开启专属人生，您的验证码是" + verifyCode;
+		javaSmsApi.sendSms(apikey, text, mobile);
+		System.out.println("sms has already been sent.");
         super.serveResource(resourceRequest, resourceResponse);
-	}
-
+	} 
+	
 	protected void include(String path, RenderRequest renderRequest,
 			RenderResponse renderResponse) throws IOException, PortletException {
 
