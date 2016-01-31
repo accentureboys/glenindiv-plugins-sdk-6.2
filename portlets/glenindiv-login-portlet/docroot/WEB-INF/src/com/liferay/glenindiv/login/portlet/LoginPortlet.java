80d27c096446eb7729ec4f9cc7d7a71757c2fbc8
Jason Xiang <494312263@qq.com>
2016-01-31 21:11:16 +0800

    Add jquery js and some necessary jsp code

---
+++ portlets/glenindiv-login-portlet/docroot/WEB-INF/src/com/liferay/glenindiv/login/portlet/LoginPortlet.java
@@ -0,0 +1,11 @@
+package com.liferay.glenindiv.login.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class LoginPortlet
+ */
+public class LoginPortlet extends MVCPortlet {
+ 
+
+}
---
+++ portlets/glenindiv-login-portlet/docroot/html/login/view.jsp
@@ -0,0 +1,11 @@
+<%@include file="/html/init.jsp" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<body>
+<!--main content start-->
+  <main>
+  
+  
+  </main>
+
+</body>
